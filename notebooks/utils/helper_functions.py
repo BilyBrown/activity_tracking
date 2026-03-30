91db11690b625bdca24aedadec5c3f78aa1b1cc1
bily <bilyhasemail@gmail.com>
2026-03-29 19:19:29 -0600

    updating date_fix helper function

--- notebooks/utils/helper_functions.py
+++ notebooks/utils/helper_functions.py
@@ -5,7 +5,7 @@ def fix_date(date_val):
     '''
     Fixes and converts the date from my google sheets to datetime.
     '''
-    if isinstance(date_val, str)
+    if isinstance(date_val, str):
         regex_pattern = r'(\d{2})[.\s]?(\d{1,2})[.\s]?(\d{1,2})'
         match = re.search(regex_pattern, str(date_val))
 
